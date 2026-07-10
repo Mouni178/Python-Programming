64c9588c13bbb19458e75d8c10abd4fad2e8b84f
Mounika <potthalammounika17@gmail.com>
2026-07-10 20:57:13 +0530

    Added Day 7 collections programs

---
+++ Python/Day_7-Collections/Tuple.py
@@ -0,0 +1,9 @@
+colors = ("Red","Green","Blue","Yellow")
+print(colors)
+print(colors[0])
+print(colors[-1])
+print(colors[1:3])
+print(len(colors))
+print("Red" in colors)
+for color in colors:
+    print(color)
