abe667e6f96a9a9a687fd6f8137439a69997b5e7
Mounika <potthalammounika17@gmail.com>
2026-07-11 18:40:26 +0530

    Added Day 8 programs

---
+++ Python/Day_8-Collections2/Sets.py
@@ -0,0 +1,15 @@
+fruits={"Apple","Orange","Guava","Watermelon","DragonFruit","Banana"}
+print(fruits)
+# Add an element
+fruits.add("Pineapple")
+print(fruits)
+# Remove an element
+fruits.remove("Banana")
+print(fruits)
+# Discard (No error if element not found)
+fruits.discard("Watermelon")
+print(fruits)
+# Clear
+temp = {1,2,3}
+temp.clear()
+print(temp)
