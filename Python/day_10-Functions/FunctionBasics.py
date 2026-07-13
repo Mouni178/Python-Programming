a6b538e3fab1588715bc403cc6e1c6fedd0b47d4
Mounika <potthalammounika17@gmail.com>
2026-07-13 18:50:46 +0530

    Added Day_9 Programs

---
+++ Python/day_10-Functions/FunctionBasics.py
@@ -0,0 +1,35 @@
+#Without Functions
+print("Welcome")
+print("Hello Mounika")
+print("Welcome")
+print("Hello Mounika")
+#With Functions
+def greet():
+    print("Welcome")
+    print("Helloo")
+greet()
+greet() 
+'''Syntax
+def functionname():
+    statement'''
+def welcome():
+    print("Welcome")
+welcome() #Func call
+welcome() #Func call
+welcome() #Func call
+#FUnction with many print
+def student():
+    print("Name : Mounika")
+    print("Age : 21")
+    print("College : KARE")
+student()
+#Local Variable
+def demo():
+    x = 10
+    print(x)
+demo()
+#Global Variable
+x = 50
+def demo():
+    print(x)
+demo()
---
+++ Python/day_10-Functions/FunctionWithArguments.py
@@ -0,0 +1,24 @@
+#Function with One Argument
+def greet(name):
+    print("Hello",name)
+greet("Mounika")
+greet("Jayanth")
+print()
+#Function with Two Arguments
+def add(a,b):
+    print(a+b)
+add(10,30)
+add(14,8)
+print()
+#Function with Three Arguments
+def student(name,age,college):
+    print(name)
+    print(age)
+    print(college)
+student("Mounika",21,"KARE")
+print()
+#Return Statement
+def add(a,b):
+    return a+b
+result = add(10,20)
+print(result)
