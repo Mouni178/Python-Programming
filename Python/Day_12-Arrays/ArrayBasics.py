fe20ba2c637fbb7b3e5574e2387b61b959f929a5
Mounika <potthalammounika17@gmail.com>
2026-07-15 22:43:56 +0530

    Added Day_12 programs

---
+++ Python/Day_12-Arrays/ArrayBasics.py
@@ -0,0 +1,15 @@
+#creating array
+marks = [90, 80, 75, 95, 88]
+print(marks)
+#Array of strings
+fruits = ["Apple", "Banana", "Orange"]
+print(fruits)
+#Array of floats
+prices = [10.5, 20.7, 30.2]
+print(prices)
+#Mixed Array (Possible in Python Lists)
+data = [10, "Mounika", 9.01, True]
+print(data)
+#Length of array
+marks = [90, 80, 75, 95, 88]
+print(len(marks))
