b85a58cde527b02e107e688c9b84d1e81c67ff22
Mounika <potthalammounika17@gmail.com>
2026-07-16 19:41:19 +0530

    Added Day_13 Programs

--- Python/Day_13-Numpy/NumPyProperties.py
+++ Python/Day_13-Numpy/NumPyProperties.py
@@ -1,1 +1,20 @@
 import numpy as np
+arr=np.array([[12,13,14],
+             [56,35,17]])
+print("Array:",arr)
+#array dimiension
+print("Array dimension:",arr.ndim)
+#no of rows and colums
+print("Shape:",arr.shape)
+#No of elements
+print("Size:",arr.size)
+#Data Type
+print("Data Type:",arr.dtype)
+#No of bytes
+print("No of Bytes:",arr.itemsize)
+#Memory used by array
+print("Memory occupied by Array:",arr.nbytes)
+#Type of Array
+print("Type of Array:",type(arr))
+#Length of array
+print("Length of Array:",len(arr))
