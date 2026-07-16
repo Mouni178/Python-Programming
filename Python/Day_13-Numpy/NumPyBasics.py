03635a3ad99b2df5c5acd1f5cc9cb6979c72a547
Mounika <potthalammounika17@gmail.com>
2026-07-16 19:23:50 +0530

    Added Day_13 Programs

---
+++ Python/Day_13-Numpy/NumPyBasics.py
@@ -0,0 +1,29 @@
+#Importing numPy
+import numpy as np
+arr = np.array([10,20,30,40])
+print(arr)
+arr2=np.array(["Apple","banana","Cherry"])
+print(arr2)
+arr3=np.array([1.2,3.4,5.6])
+print(arr3)
+#numPyCreation
+#1D Array
+arr4=np.array([1,2,3,4])
+print("\n DArray")
+print(arr4)
+#2D Array
+arr5=np.array([[1,2],[3,4],[5,6]])
+print("\n2d Array")
+print(arr5)
+#3D Array
+arr6=np.array([[[1,2],[3,4],[5,6],[7,8]]])
+print("\n3D Array")
+print(arr6)
+#Zeroes
+arr7=np.zeros((2,3))
+print("\nArrays with Zeroes")
+print(arr7)
+#ones
+arr8=np.ones((3,3))
+print("\nArrays with ones")
+print(arr8)
