d22cfc2ae93652e386210c15e8f66d657278548c
Mounika <potthalammounika17@gmail.com>
2026-07-18 21:43:22 +0530

    Added Day_15 Programs

---
+++ Python/Day_15-ArrayExamples/Examplw1.py
@@ -0,0 +1,23 @@
+#Print all elements
+import numpy as np
+arr = np.array([10, 20, 30, 40, 50])
+for i in arr:
+    print(i)
+#Find the Sum of Elements
+import numpy as np
+arr = np.array([5, 10, 15, 20, 25])
+total = np.sum(arr)
+print("Sum =", total)
+#Find the Largest Element
+import numpy as np
+arr = np.array([25, 40, 15, 80, 60])
+print("Largest =", np.max(arr))
+#Find the Smallest Element
+import numpy as np
+arr = np.array([25, 40, 15, 80, 60])
+print("Smallest =", np.min(arr))
+#Find the Average
+import numpy as np
+arr = np.array([10, 20, 30, 40, 50])
+average = np.mean(arr)
+print("Average =", average)
