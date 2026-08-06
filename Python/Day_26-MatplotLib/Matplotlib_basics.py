84ba08f54f39be4900c71d57a0b8fcd46da6daea
Mounika <potthalammounika17@gmail.com>
2026-08-06 22:12:01 +0530

    Added Day_26 Programs

---
+++ Python/Day_26-MatplotLib/Matplotlib_basics.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+# What is Matplotlib?
+# Matplotlib is a Python library used for creating graphs and charts.
+
+# Why Matplotlib?
+# - Data Visualization
+# - Easy to understand data
+# - Supports different types of plots
+# - Widely used in Data Science and Machine Learning
+
+# Create X and Y values
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 30, 40, 50]
+
+# Simple Line Plot
+plt.plot(x, y)
+plt.show()
